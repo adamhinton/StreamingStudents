e597b847208a1e894f4be85d42aba7849b88a7ae
adamd <adam.d.hinton@gmail.com>
2024-04-07 12:27:22 -0400

    boilerplate

--- src/dev/lpa/MainOptional.java
+++ src/dev/lpa/MainOptional.java
@@ -1,4 +1,20 @@
 package dev.lpa;
 
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
 public class MainOptional {
+
+    Course pymc= new Course("PYMC", "Python Masterclass", 50);
+    Course jmc= new Course("JMC", "Java Masterclass", 100);
+
+    List<Student> myStudents = Stream
+            .generate(() -> Student.getRandomStudent(jmc, pymc))
+            .limit(5000)
+            // Doing it this way so we can modify this list
+            .collect(Collectors.toList());
+
+
+
 }
