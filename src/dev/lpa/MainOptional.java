a7486d3c7a3c9fbab0910667527508438f90af54
adamd <adam.d.hinton@gmail.com>
2024-04-07 17:59:02 -0400

    isPresent and ifPresent

--- src/dev/lpa/MainOptional.java
+++ src/dev/lpa/MainOptional.java
@@ -1,5 +1,6 @@
 package dev.lpa;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 import java.util.Random;
@@ -19,26 +20,37 @@ public class MainOptional {
                 .collect(Collectors.toList());
 
 
-        Optional<Student> o1 = getStudent(null, "first");
+        Optional<Student> o1 = getStudent(new ArrayList<>(),"first");
         System.out.println("Empty = " + o1.isEmpty() + ", Present = " + o1.isPresent());
         System.out.println(o1);
+
+
+//        System.out.println(o1.get());
+        o1.ifPresent(System.out::println);
+
+        Optional<Student> o2 = getStudent(myStudents,"first");
+        System.out.println("Empty = " + o2.isEmpty() + ", Present = " + o2.isPresent());
+        System.out.println(o2);
+
+        o2.ifPresent(System.out::println);
+
     }
 
     // `type` is type of retrieval, which is first, last or any
-    // Any method retuning an Optional should never return null. Instead it should return an Optional.
+    // Any method retuning an Optional should never return null. Instead it should return an empty Optional.
     private static Optional<Student> getStudent(List<Student> list, String type){
 
         if (list == null || list.size() == 0){
-            return null;
+            return Optional.empty();
         }
         else if (type.equals("first")){
-            return Optional.of(list.get(0));
+            return Optional.ofNullable(list.get(0));
         }
         else if (type.equals("last")){
-            return Optional.of(list.get(list.size() - 1));
+            return Optional.ofNullable(list.get(list.size() - 1));
         }
 
-        return Optional.of(list.get(new Random().nextInt(list.size())));
+        return Optional.ofNullable(list.get(new Random().nextInt(list.size())));
     }
 
 }
